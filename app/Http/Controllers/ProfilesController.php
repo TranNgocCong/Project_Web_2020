ea57e3a608e6ed80e11abdb239141e2fc734d410
TranNgocCong <55443845+TranNgocCong@users.noreply.github.com>
2021-01-21 12:52:16 +0700

    Update ProfilesController.php

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -74,7 +74,7 @@ class ProfilesController extends Controller
         if (request('image')) {
            
                 $file = $request->file('image');
-                $name = time() . $file->getClientOriginalName()->fit(300,300);
+                $name = time() . $file->getClientOriginalName();
                 $filePath = 'images/stories/' . $name;
                 $path=Storage::disk('s3')->put($filePath, file_get_contents($file));
                 $imagePath ='https://vivu1.s3.amazonaws.com/images/stories/'.$name;
